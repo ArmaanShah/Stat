73345154b3094548699538de1b7a9e9193114f4e
Armaan Shah <69207811+ArmaanShah@users.noreply.github.com>
2024-12-27 15:25:57 -0500

    Update Stat.java

--- Stat.java
+++ Stat.java
@@ -1,7 +1,6 @@
 /*
  * Stat.java
  * Author:  Armaan Shah
- * Submission Date: November 15th 2024
  *
  *This program does the following:
  *The following code defines a Stat class that stores an array of double values 
@@ -9,19 +8,6 @@
  *finding the min, max, average, mode, variance and standard deviation amongst other things. 
  * Statement of Academic Honesty:
  *
- * The following code represents my own work. I have neither 
- * received nor given inappropriate assistance. I have not copied 
- * or modified code from any source other than the course webpage 
- * or the course textbook. I recognize that any unauthorized 
- * assistance or plagiarism will be handled in accordance with 
- * the University of Georgia's Academic Honesty Policy and the 
- * policies of this course. I recognize that my work is based 
- * on an assignment created by the School of Computing 
- * at the University of Georgia. Any publishing or
- * posting of source code for this assignment is strictly 
- * prohibited unless you have written consent from the 
- * School of Computing at the University of Georgia.  
- */
 public class Stat {
 
 	private double[] data;// Array to hold data values
